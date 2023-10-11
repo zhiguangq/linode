237c1f2ac4350bd816a4f862c4c8f4e6a69997c7
zhiguangq <qiu_zhi_guang@126.com>
2023-10-11 11:46:51 +0800

    Update run.sh
    a

--- run.sh
+++ run.sh
@@ -3,6 +3,13 @@ sudo apt-get install -y nginx
 cp -rf ./nginx-default /etc/nginx/sites-available/default
 service nginx restart
 
+#install ffmpeg
+apt-get install -y ffmpeg
+
+#install yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
+
 
 
 
