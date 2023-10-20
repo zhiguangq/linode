0345969b6a4a861275015fe6bf10297b090a875d
zhiguangq <qiu_zhi_guang@126.com>
2023-10-20 23:18:49 +0800

    Update run.sh
    add

--- run.sh
+++ run.sh
@@ -10,6 +10,22 @@ apt-get install -y ffmpeg
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 chmod a+rx /usr/local/bin/yt-dlp
 
+#install go
+wget https://dl.google.com/go/go1.20.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
+echo export PATH=$PATH:/usr/local/go/bin >> ~/.profile
+echo go env -w GO111MODULE=on >> ~/.profile
+source ~/.profile
+mkdir ~/go
+
+cd go
+go install github.com/go-kratos/kratos/cmd/kratos/v2@latest
+git clone https://github.com/zhiguangq/youtubeFile.git
+go build -o ./bin/ ./...
+nohup ./bin/linode -conf ./configs/ >a.log 2>&1 &
+
+
+
 
 
 
